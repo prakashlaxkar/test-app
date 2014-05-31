7f7a03604b5c5298dc5d4917b8f3140039a5827e
Animesh <animeshjaain@gmail.com>
2014-05-31 14:30:54 +0530

    configured bootstrap with rails added all required js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require jquery-1.7.1.min
+//= require jquery-ui-1.8.1.custom.min
+//= require scripts
+//= require_directory.
